64d3f8401090620f09e4babf395be7bd79f5a209
Armando Aranda <arandaae19@gmail.com>
2022-04-21 21:59:13 -0500

    First Commmit

---
+++ HelloWorld_2.0/HelloWorld_2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace HelloWorld_2._0
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "";
+            string age = "";
+
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+
+            Console.WriteLine("How old are you: ");
+            age = Console.ReadLine();
+
+            Console.WriteLine($"You are {age} years old.");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
